01cc3a9f4c2b7d34ceac7e3b39a6f2ffec37e4d0
Hudson <73559234+HudsonSilv2@users.noreply.github.com>
2025-04-01 14:22:20 -0300

    Update RelatorioPendencia.java

--- src/RelatorioPendencia.java
+++ src/RelatorioPendencia.java
@@ -2,7 +2,7 @@ public class RelatorioPendencia {
     public static void main(String[] args){
         String nome = "Hudson";
         String endereco = "Rua Escritor Nilo Pereira";
-        long telefone = 5584991639078L;
+        long telefone = 558499999978L;
 
 
         System.out.printf("O '%s' domiciliado no endereço '%s' e telefone +%d, não possui nenhum tipo de pendência.", nome, endereco, telefone);
